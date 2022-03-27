d885d9cdfbce394c5c924436f29263659c284aaa
duy ngeuyn <nguyentheduy494@gmail.com>
2022-03-27 20:54:56 +0700

    hehe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <div className="container">
+      <h1 className="title"><span>human</span> Generate 3000<div className="sus"></div></h1>
+    
+      <div className="aki"></div>
+  <div class="btn btn-start">
+    <a href="#">Press Start<span className="emoji">┗|｀O′|┛</span></a>
+  </div>
+  <div class="btn btn-quit">
+    <a href="https://google.com/">Quit Game <span className="emoji">(T_T)</span></a>
+  </div>
+</div>
+<a href="https://excalidraw.com/"><div className="logo"></div></a>
+<a href="https://twitter.com/suwanvipatawat"><div className="logo3"></div></a>
+<a href="https://www.facebook.com/bansuwann/"><div className="logo4"></div></a>
+<h5>© 2003-2022  duy ngủ yên/CoderSchool</h5>
+    </>
+  )
 }
 
-export default App;
+export default App
